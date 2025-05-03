8da5d06451525f67de173b6632825d67cf7d22db
Nayeemchowdhury <nayeemchowdhury2002@gmail.com>
2025-05-03 20:44:51 +0530

    second commit

---
+++ two.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"Enter your number"<<endl;
+    cin>>n;
+    for(int i=n;i>=1;i=i-1)
+    {
+        cout<<i<<"";
+    }
+}
