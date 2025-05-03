47325938c979aaab6aadc44d7ed7716ef457a9fb
Nayeemchowdhury <nayeemchowdhury2002@gmail.com>
2025-05-03 22:09:26 +0530

    Its third time

---
+++ table.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+    cout<<"Enter your table"<<endl;
+    cin>>n;
+    for(i=1;i<=10;i++)
+    {
+        cout<<n*i<<endl;
+    }
+}
