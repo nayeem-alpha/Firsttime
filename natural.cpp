99eb65be31ee73ea86f002424cbc9c380381d35d
Nayeemchowdhury <nayeemchowdhury2002@gmail.com>
2025-05-05 14:12:12 +0530

    This is my second commit for more information

--- natural.cpp
+++ natural.cpp
@@ -2,14 +2,18 @@
 using namespace std;
 int main()
 {
-    int i,n;
-    cout<<"Enter your number :"<<endl;
-    cin>>n;
-    int sum =0;
-    for(i = 1;i<=n;i++)
-    {
-        sum = sum+i;
-    }
+    // int i,n;
+    // cout<<"Enter your number :"<<endl;
+    // cin>>n;
+    // int sum =0;
+    // for(i = 1;i<=n;i++)
+    // {
+    //     sum = sum+i;
+    // }
 
-    cout<<sum;
+    // cout<<sum;
+    int n,i;
+    cout<<"Enter your Number :"<<endl;
+    cin>>n;
+    cout<<(n*(n+1)/2);
 }
